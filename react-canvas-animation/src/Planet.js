09379f9a6b6e834cfa5d56378fc53160336b3362
jehong <jehong@student.42seoul.com>
2021-04-12 02:05:58 +0900

    refactor: return canvas in the planet class

--- react-canvas-animation/src/Planet.js
+++ react-canvas-animation/src/Planet.js
@@ -1,84 +1,10 @@
-// import React from 'react';
-// import Canvas from './Canvas.js';
+import React from 'react';
+import Canvas from './Canvas.js';
 
-// class Planet extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     const {name, star, radius, color, velocity, orbitRadius, src} = props;
-//     console.log(name)
-//     this.name = name;
-//     this.star = star;
-//     this.radius = radius;
-//     this.color = color;
-//     this.velocity = velocity;
-//     this.orbitRadius = orbitRadius;
-
-//     this.theta = Math.random() * Math.PI * 2;
-//     this.clicked = false;
-//     this.mouse = {
-//       x: star.x,
-//       y: star.y,
-//     };
-
-//     this.distanceFromCenter = orbitRadius;
-//     this.x = star.x + orbitRadius * Math.cos(this.theta);
-//     this.y = star.y + orbitRadius * Math.sin(this.theta);
-//     this.img = new Image();
-//     this.img.src = src;
-
-//     // this.render();
-//   }
-
-//   animate = (ctx, stageWidth, stageHeight) => {
-//     ctx.clearRect(0, 0, stageHeight, stageHeight);
-
-//     this.update();
-//     this.draw(ctx);
-//   }
-
-//   update = () => {
-//     if (this.clicked) {
-//       this.x = this.mouseX;
-//       this.y = this.mouseY;
-//       // console.log('mouse',planet.mouseX, planet.mouseY);
-//     } else {
-//       let star = this.find(element => element.name === this.star);
-
-//       this.x = star.x + this.orbitRadius * Math.cos(this.theta);
-//       this.y = star.y + this.orbitRadius * Math.sin(this.theta);
-//     }
-//   }
-
-//   draw = (ctx) => {
-//     ctx.beginPath();
-//     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-//     ctx.fillStyle = this.color;
-//     ctx.fill();
-//     ctx.closePath();
-//   }
- 
-//   // render() {
-//   // return (
-//   //   <>
-//   //   <div>Hello</div>
-//   //   <Canvas 
-//   //     draw={this.animate} 
-//   //     id={this.name}
-//   //     type='dynamic' 
-//   //   />
-//   //   </>
-//   // );
-//   // }
-// }
-
-// export default Planet;
-
-
-
-export default class Planet {
+class Planet extends React.Component {
   constructor(props) {
-    const {name, star, x, y, radius, color, velocity, orbitRadius, src} = props;
-    console.log(name, star)
+    super(props);
+    const {name, star, x, y, radius, color, velocity, orbitRadius, src} = props.props;
     
     this.name = name;
     this.star = star;
@@ -89,14 +15,10 @@ export default class Planet {
 
     this.theta = Math.random() * Math.PI * 2;
     this.clicked = false;
-    this.mouse = {
-      x: x,
-      y: y,
-    };
     this.star = {
       x: x,
       y: y,
-    }
+    };
 
     this.distanceFromCenter = orbitRadius;
     this.x = this.star.x + orbitRadius * Math.cos(this.theta);
@@ -107,26 +29,27 @@ export default class Planet {
     // this.render();
   }
 
-  animate = (ctx, stageWidth, stageHeight, planets) => {
+  animate = (ctx, stageWidth, stageHeight) => {
     ctx.clearRect(0, 0, stageHeight, stageHeight);
 
-    this.update(planets);
+    this.update();
     this.draw(ctx);
   }
 
-  update = (planets) => {
-    this.theta += this.velocity;
-    // console.log(this.x, this.y)
+  update = () => {
     // if (this.clicked) {
     //   this.x = this.mouseX;
     //   this.y = this.mouseY;
     //   // console.log('mouse',planet.mouseX, planet.mouseY);
     // } else {
-    //   // let star = planets.find(element => element.name === this.star);
+    //   let star = this.find(element => element.name === this.star);
 
-      this.x = this.star.x + this.orbitRadius * Math.cos(this.theta);
-      this.y = this.star.y + this.orbitRadius * Math.sin(this.theta);
+    //   this.x = star.x + this.orbitRadius * Math.cos(this.theta);
+    //   this.y = star.y + this.orbitRadius * Math.sin(this.theta);
     // }
+    this.theta += this.velocity;
+    this.x = this.star.x + this.orbitRadius * Math.cos(this.theta);
+    this.y = this.star.y + this.orbitRadius * Math.sin(this.theta);
   }
 
   draw = (ctx) => {
@@ -136,4 +59,21 @@ export default class Planet {
     ctx.fill();
     ctx.closePath();
   }
+ 
+  render() {
+  return (
+    <>
+    <div>Hello</div>
+    <Canvas 
+      draw={this.animate} 
+      id={this.name}
+      type='dynamic' 
+    />
+    </>
+  );
+  }
 }
+
+export default Planet;
+
+
--- react-canvas-animation/src/SolarSystem.js
+++ react-canvas-animation/src/SolarSystem.js
@@ -6,25 +6,23 @@ import Canvas from './Canvas.js';
 export default function SolarSystem() {
   const planets = usePlanetState();
   // planets.map(planet => new Planet(planet.name, planet.star, planet.radius, planet.color, planet.velocity, planet.orbitRadius, planet.src))
-  let q = planets.map(planet => new Planet(planet))
+  // let q = planets.map(planet => new Planet(planet))
   // let q = [];
   // for (let planet in planets) {
   //   q.push(new Planet(planet));
   // }
 
-  for (let i of q) {
-    console.log(i.star)
-  }
 
   return (
     <>
-    {q.map(planet => <Canvas 
+    {planets.map(planet => <Planet props={planet} key={planet.name}/>)}
+    {/* {q.map(planet => <Canvas 
     key={planet.name}
     draw={planet.animate}
     planets={q} 
     id={planet.name}
     type='dynamic' 
-  />)}
+  />)} */}
     </>
     
   );
